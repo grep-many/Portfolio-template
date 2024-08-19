5a39c0d518919247e7907b60d5c8bea5b2ea45c8
Manish <manishkumar9540898@gmail.com>
2024-08-19 11:43:01 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# John Doe - Portfoliio
+scss files which compiled to create css files to design webiste's . For compiling css i used the live sass compiler a vs code extension which compiles scss files to css file ,Which further used by index.html file by including the css output file of scss file. 
+
+
+## Note
+* This is a static website
+* It does not have any backend functionality
+* Used scss files are in scss folder
+* The static website part is in the dist file
---
+++ dist/js/main.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded',()=>{
+
+    const menuBtn = document.querySelector('.menu-btn');
+    const hamBurger = document.querySelector('.menu-btn_burger');
+    const nav = document.querySelector('.nav');
+    const menuNav = document.querySelector('.menu-nav');
+    const navitems = document.querySelectorAll('.menu-nav_item');
+
+    let showMenu = false;
+     
+    menuBtn.addEventListener('click',toggleMenu);
+
+    function toggleMenu(){
+        if(!showMenu){
+            hamBurger.classList.add('open');
+            nav.classList.add('open');
+            menuNav.classList.add('open');
+            navitems.forEach(item => item.classList.add('open'));
+
+            showMenu = true;
+        }
+        else{
+            hamBurger.classList.remove('open');
+            nav.classList.remove('open');
+            menuNav.classList.remove('open');
+            navitems.forEach(item => item.classList.remove('open'));
+
+            showMenu = false;
+        }
+    }
+})
